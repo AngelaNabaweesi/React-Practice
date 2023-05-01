0b6ab175dd67cd1a9e0705c5047d30b997bdef9f
Angela <nabaweesi3579@gmail.com>
2023-05-01 14:53:13 +0300

    Feat: LyricsPage

---
+++ real/react/beginner/src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+import Header from './lyrics/Header';
+
+
+function App() {
+  return (
+    <Header />
+  );
+}
+
+export default App;
---
+++ real/react/beginner/src/lyrics/Header.js
@@ -0,0 +1,84 @@
+function Header(){
+    return (
+      <div>
+        <h1>So Will I (100 Billion X)</h1><br />
+        <p>Words and Music by Joel Houston Benjamin <br/>Hastings & Michael Fatkin</p><br />
+        <p>© 2017 Hillsong Music Publishing CCLI: 7084123</p><br />
+        <div className="div2">
+          <div className="h1heading">VERSE 1</div>
+          <p>God of creation<br/>
+          There at the start<br/>
+          Before the beginning of time<br/>
+          With no point of reference<br/>
+          You spoke to the dark<br/>
+          And fleshed out the wonder of light</p><br />
+  
+          <div className="h1heading">CHORUS 1</div>
+          <p>And as You speak<br/>
+          A hundred billion galaxies are born<br/>
+          In the vapour of Your breath the planets form<br/>
+          If the stars were made to worship so will I<br/>
+          I can see Your heart in everything You’ve made<br/>
+          Every burning star<br/>
+          A signal fire of grace<br/>
+          If creation sings Your praises so will I</p><br/>
+  
+          <div className="h1heading">VERSE 2</div>
+          <p>God of Your promise<br/>
+          You don’t speak in vain<br/>
+          No syllable empty or void<br/>
+          For once You have spoken<br/>
+          All nature and science<br/>
+          Follow the sound of Your voice</p><br/>
+  
+          <div className="h1heading">CHORUS</div>
+          <p>And as You speak<br/>
+          A hundred billion creatures catch Your breath<br/>
+          Evolving in pursuit of what You said<br/>
+          If it all reveals Your nature so will I<br/>
+          I can see Your heart in everything You say<br/>
+          Every painted sky<br/>
+          A canvas of Your grace<br/>
+          If creation still obeys You so will I</p><br/>
+  
+          <div className="h1heading">BRIDGE</div>
+          <p >If the stars were made to worship so will I<br/>
+          If the mountains bow in reverence so will I<br/>
+          If the oceans roar Your greatness so will I<br/>
+          For if everything exists to lift You high so will I<br/>
+          If the wind goes where You send it so will I<br/>
+          If the rocks cry out in silence so will I<br/>
+          If the sum of all our praises still falls shy<br/>
+          Then we’ll sing again a hundred billion times</p><br/>
+  
+          <div className="h1heading">VERSE 3</div>
+          <p>God of salvation<br/>
+          You chased down my heart<br/>
+          Through all of my failure and pride<br/>
+          On a hill You created<br/>
+          The light of the world<br/>
+          Abandoned in darkness to die</p><br/>
+  
+          <div className="h1heading">CHORUS 3</div>
+          <p>And as You speak<br/>
+          A hundred billion failures disappear<br/>
+          Where You lost Your life so I could find it here<br/>
+          If You left the grave behind You so will I<br/>
+          I can see Your heart in everything You’ve done<br/>
+          Every part designed in a work of art called love<br/>
+          If You gladly chose surrender so will I<br/>
+          I can see Your heart<br/>
+          Eight billion different ways<br/>
+          Every precious one<br/>
+          A child You died to save<br/>
+          If You gave Your life to love them so will I</p><br/>
+  
+          <div className="h1heading">TAG</div>
+          <p>Like You would again a hundred billion times<br/>
+          But what measure could amount to Your desire<br/>
+          You’re the One who never leaves the one behind</p>
+        </div>
+      </div>
+    )
+  }
+  export default Header
